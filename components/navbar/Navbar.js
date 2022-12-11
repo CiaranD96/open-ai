e01d8f0357c45c8578d7ddb3dd52d70e2889502c
Ciaran <ciaran.donnachie@gmail.com>
2022-12-11 20:28:01 +0000

    add materialize icons

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  useEffect(() => {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
   return (
-    <nav>
-      <div className='nav-wrapper blue lighten-1'>
-        <Link href='/' className='brand-logo'>
-          OpenAI
-        </Link>
-        <ul id='nav-mobile' className='right hide-on-med-and-down'>
-          <li>
-            <Link href='/imageGenerator'>Image Generator</Link>
-          </li>
-          <li>
-            <Link href='/textCompletion'>Text Completion</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Fragment>
+      <nav>
+        <div className='nav-wrapper'>
+          <Link href='/' className='brand-logo'>
+            OpenAI
+          </Link>
+          <Link href='#' data-target='mobile-demo' className='sidenav-trigger'>
+            <i className='material-icons'>menu</i>
+          </Link>
+          <ul className='right hide-on-med-and-down'>
+            <li>
+              <Link href='/imageGenerator'>Image Generator</Link>
+            </li>
+            <li>
+              <Link href='/textCompletion'>Text Completion</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className='sidenav' id='mobile-demo'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/imageGenerator'>Image Generator</Link>
+        </li>
+        <li>
+          <Link href='/textCompletion'>Text Completion</Link>
+        </li>
+      </ul>
+    </Fragment>
   );
 };
 
